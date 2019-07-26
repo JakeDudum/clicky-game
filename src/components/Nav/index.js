20c2cf6e97381b0c6c590cc4a5f7787639fb90cd
Jake Dudum <jakedudum@gmail.com>
2019-07-25 18:06:46 -0700

    Created basic navbar with no components

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,2 +1,14 @@
 import React from "react";
 import "./style.css";
+
+function Navbar() {
+    return (
+        <nav className="navbar">
+            <div>Clicky Game</div>
+            <div>Click an Image to Start</div>
+            <div>Score</div>
+        </nav>
+    );
+}
+
+export default Navbar;
