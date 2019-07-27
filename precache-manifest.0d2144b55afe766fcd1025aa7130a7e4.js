77ff80d965c303e942001332bd8cbda8a6fa89cb
Jake Dudum <jakedudum@gmail.com>
2019-07-26 18:01:24 -0700

    Updates

---
+++ precache-manifest.0d2144b55afe766fcd1025aa7130a7e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3eae14718e51f83b36c0eb2e5378ed6",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "a88cd0f68ec6d22c3e22",
+    "url": "/clicky-game/static/css/main.1de53a40.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/clicky-game/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "a88cd0f68ec6d22c3e22",
+    "url": "/clicky-game/static/js/main.7cd93700.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  },
+  {
+    "revision": "82848488b73a87d72f2436c02b9ab608",
+    "url": "/clicky-game/static/media/one-piece-font.82848488.ttf"
+  }
+]);
